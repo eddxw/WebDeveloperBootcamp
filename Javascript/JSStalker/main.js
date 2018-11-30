1ff724be6b1bfeda6e38e78bee388390b8064d65
eddxw <eduardo.jemb@gmail.com>
2018-11-30 02:57:25 -0700

    JS Stalker Exercise

---
+++ Javascript/JSStalker/main.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?", "John");
+var lastName = prompt("What is your last name?", "Doe");
+var age = prompt("What is your age?", "23");
+
+console.log(`Your full name is ${firstName} ${lastName}`);
+console.log(`You are ${age} years old`);
+
