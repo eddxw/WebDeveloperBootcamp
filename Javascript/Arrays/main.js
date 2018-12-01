b14ce7cad0c1c0028abf76355e85e01acbffed81
eddxw <eduardo.jemb@gmail.com>
2018-12-01 04:36:21 -0700

    updated todo list

--- Javascript/Arrays/main.js
+++ Javascript/Arrays/main.js
@@ -2,14 +2,34 @@ window.onload = function () {
     var todos = ["Buy new turtle"];
     var input = prompt("What would you like to do?");
 
+    function listTodos(){
+        console.log('**********');
+        todos.forEach((todo, idx) => {
+            console.log(`${idx}: ${todo}`);
+        })
+        console.log('**********');
+    }
+    function newTodo(){
+        var newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+        console.log("Added Todo");
+    }
+    function deleteTodo(){
+        let index = prompt("Enter index of todo to delete");
+        todos.splice(index,1);
+        console.log("Deleted Todo");
+    }
+    
     while (input !== "quit") {
         if (input === "list") {
-            console.log(todos);
+            listTodos();
         } else if (input === "new") {
-            var newTodo = prompt("Enter new todo");
-            todos.push(newTodo);
+            newTodo();
+        } else if (input === "delete") {
+            deleteTodo();
         }
         var input = prompt("What would you like to do?");
     }
     console.log("Ok, you quit the app");
 }
+
