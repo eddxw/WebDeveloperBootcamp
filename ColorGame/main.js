4b4ea5a89aaf18633459dd83a889e7e233e2e7e7
eddxw <eduardo.jemb@gmail.com>
2018-12-03 03:08:21 -0700

    add radom pickColor

--- ColorGame/main.js
+++ ColorGame/main.js
@@ -8,8 +8,9 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,11 +20,29 @@ for (let i = 0; i < squares.length; i++) {
 
     //add click listeners to squares
     squares[i].addEventListener("click", function () {
+        // grab color of clicked square
         var clickedColor = this.style.backgroundColor;
+
+        //compare color to pickedColor
         if (clickedColor === pickedColor) {
-            alert("correct");
+            messageDisplay.textContent = "Correct!!";
+            changeColors(clickedColor);
         } else {
-            alert("wrong");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color) {
+    // loop through all squares
+    for (let i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor((Math.random() * colors.length));
+
+    return colors[random];
 }
