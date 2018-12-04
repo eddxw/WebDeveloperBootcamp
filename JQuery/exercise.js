d80f1263fa916ede62bf2b6708c0fd7cd4557828
eddxw <eduardo.jemb@gmail.com>
2018-12-03 19:21:41 -0700

    Jquery exercise1

---
+++ JQuery/exercise.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("div").css("background", "purple");
+    $("div.highlight").css("width", "200px");
+    $("#third").css("border", "1px solid orange");
+    $("div:nth-of-type(1)").css("color", "pink");
+});
